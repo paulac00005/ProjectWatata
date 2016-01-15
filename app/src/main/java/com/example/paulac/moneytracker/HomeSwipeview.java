eb021ae50018c978b5a564f777f246c52ded106e
paulac00005 <lithiumer13@gmail.com>
2016-01-15 14:32:48 +0800

    category and home swipe okay

--- app/src/main/java/com/example/paulac/moneytracker/HomeSwipeview.java
+++ app/src/main/java/com/example/paulac/moneytracker/HomeSwipeview.java
@@ -1,6 +1,7 @@
 package com.example.paulac.moneytracker;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,7 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class HomeSwipeview extends Fragment {
+public class HomeSwipeview extends ListFragment {
 
     private String title;
     private int page;
@@ -21,6 +22,7 @@ public class HomeSwipeview extends Fragment {
         super.onCreate(savedInstanceState);
         page = getArguments().getInt("someInt", 0);
         title = getArguments().getString("someTitle");
+
     }
 
     public static HomeSwipeview newInstance(int page, String title) {
@@ -51,46 +53,12 @@ public class HomeSwipeview extends Fragment {
                 "Android Example",
                 "List View Source Code",
                 "List View Array Adapter",
-                "Android Example List View"
-        };
+                "Android Example List View"};
 
-        // Define a new Adapter
-        // First parameter - Context
-        // Second parameter - Layout for the row
-        // Third parameter - ID of the TextView to which the data is written
-        // Forth - the Array of data
+        this.setListAdapter(new ArrayAdapter<String>(
+                this, R.layout.listviewlayout,
+                R.id.Itemname, itemname));
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, values);
-
-
-        // Assign adapter to ListView
-        listView.setAdapter(adapter);
-
-        // ListView Item Click Listener
-        listView.setOnItemClickListener(new OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-
-                // ListView Clicked item index
-                int itemPosition     = position;
-
-                // ListView Clicked item value
-                String  itemValue    = (String) listView.getItemAtPosition(position);
-
-                // Show Alert
-                Toast.makeText(getApplicationContext(),
-                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
-                        .show();
-
-            }
-
-        });
-    }
-
-}
 
 tvLabel.setText(page + " -- " + title);
         return view;
