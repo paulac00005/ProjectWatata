ce1655991f284f2416b02bf342561bab7c3286ea
paulac00005 <lithiumer13@gmail.com>
2016-01-15 14:11:03 +0800

    category and home swipe okay

--- app/src/main/java/com/example/paulac/moneytracker/Home.java
+++ app/src/main/java/com/example/paulac/moneytracker/Home.java
@@ -3,6 +3,9 @@ package com.example.paulac.moneytracker;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Home extends FragmentActivity {
 
@@ -12,6 +15,7 @@ public class Home extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ViewPager vp = (ViewPager)findViewById(R.id.HomeViewPager);
+
         HomeSwipeAdapter HomeAdapter = new HomeSwipeAdapter(getSupportFragmentManager());
         vp.setAdapter(HomeAdapter);
     }
--- app/src/main/java/com/example/paulac/moneytracker/HomeSwipeview.java
+++ app/src/main/java/com/example/paulac/moneytracker/HomeSwipeview.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -40,7 +41,58 @@ public class HomeSwipeview extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_home_swipview, container, false);
         TextView tvLabel = (TextView) view.findViewById(R.id.HomeSwipetextView);
-        tvLabel.setText(page + " -- " + title);
+        ListView listView = (ListView) view.findViewById(R.id.listView);
+
+        // Defined Array values to show in ListView
+        String[] values = new String[] { "Android List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android Example List View"
+        };
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition     = position;
+
+                // ListView Clicked item value
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                        .show();
+
+            }
+
+        });
+    }
+
+}
+
+tvLabel.setText(page + " -- " + title);
         return view;
     }
 }
