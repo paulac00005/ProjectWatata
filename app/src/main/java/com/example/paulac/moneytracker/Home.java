3635f0b55e9f4768ff7dc04c6727a78c17d40cba
paulac00005 <lithiumer13@gmail.com>
2016-01-21 14:00:29 +0800

    categories listview try

--- app/src/main/java/com/example/paulac/moneytracker/Home.java
+++ app/src/main/java/com/example/paulac/moneytracker/Home.java
@@ -1,16 +1,8 @@
 package com.example.paulac.moneytracker;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.view.View;
-import android.view.Window;
-import android.widget.Button;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 public class Home extends FragmentActivity {
 
