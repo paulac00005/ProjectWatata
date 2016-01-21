cec0f879473da2f42301293be2a4e0312bb32d32
paulac00005 <lithiumer13@gmail.com>
2016-01-21 14:21:17 +0800

    Home(AppCompatActivity)

--- app/src/main/java/com/example/paulac/moneytracker/Home.java
+++ app/src/main/java/com/example/paulac/moneytracker/Home.java
@@ -1,10 +1,13 @@
 package com.example.paulac.moneytracker;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
-public class Home extends FragmentActivity {
+public class Home extends AppCompatActivity{
 
 
     @Override
